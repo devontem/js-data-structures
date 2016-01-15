af07c67fcc5ee992e5b1d3016c3301e72248352f
Devonte M <devontem@live.com>
2016-01-15 11:31:50 -0800

    completed linkedlist.js & tree.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if (!list.tail){
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    if (list.head){
+      var oldHead = list.head;
+      list.head = list.head.next;
+      return oldHead.value;
+    }
   };
 
   list.contains = function(target){
+    var newVariable = list.head;
+    if (list.tail.value === target) { return true };
+    while (newVariable.next) {
+      if (newVariable.value === target) {
+        return true;
+      }
+      newVariable = newVariable.next;
+    }
+    return false;
   };
 
   return list;
